5547bd1078ee06c91424f25f0632e7c300b1bb9e
中华田园犬 <tcstory@163.com>
2016-09-01 23:13:48 +0800

    添加webpack.prod.config.js文件

---
+++ webpack.prod.config.js
@@ -0,0 +1,65 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var autoprefixer = require('autoprefixer');
+var postPxToEm = require('postcss-px-to-em');
+
+
+module.exports = {
+    entry: {
+        app: ['./src/index.js']
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: '/',
+        filename: 'js/[name].[chunkhash:6].js',
+        chunkFilename: "js/chunk/[name].[chunkhash:6].js"
+    },
+    resolveLoader: {
+        root: path.join(__dirname, 'node_modules')
+    },
+    resolve: {
+        extensions: ['', '.js'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url?limit=10000&name=images/[name].[hash:6].[ext]'
+            },
+            {
+                test: /\.(woff|eot|ttf).*?$/i,
+                loader: 'url?limit=10000&name=fonts/[name].[hash:6].[ext]'
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css!postcss')
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new ExtractTextPlugin('css/[name].[chunkhash:6].css'),
+        new webpack.DefinePlugin({
+            __DEV__: false
+        })
+    ],
+    postcss: function () {
+        return [autoprefixer, postPxToEm({base: 16})];
+    }
+};
+
